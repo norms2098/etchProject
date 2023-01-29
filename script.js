48434565485ffe79f7aa3cca49e8e9cfb20d21d6
norms2098 <nrmsdgdgn@gmail.com>
2023-01-29 15:26:25 +0800

    added rainbow mode

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const sliderElement = document.getElementById("sliderGrid");
 const resetBtn = document.getElementById("resetbtn");
 const sliderLabel=document.querySelector('.slider-label');
 const clearBtn = document.getElementById("clearbtn");
+const rainbowBtn=document.getElementById("rainbowbtn");
 
 function createGrid(tot){
     for(let i=0;i<tot*tot;i++){
@@ -21,39 +22,37 @@ function createGrid(tot){
     })
 }
 
+function clearGrid(){
+    gridContainer.innerHTML="";
+    createGrid(sliderElement.value);
+}
+
+function resetGrid(){
+    gridContainer.innerHTML="";
+    sliderLabel.innerHTML=`Adjust grid size:<br>${sliderElement.defaultValue} X ${sliderElement.defaultValue}`;
+    sliderElement.value=sliderElement.defaultValue;
+    createGrid(sliderElement.defaultValue);
+}
+function rainbowModeOn(){
+    const rainbowColors=["red","orange","yellow","green","blue","purple"];
+    const cellsInGrid = document.querySelectorAll(".cells");
+    cellsInGrid.forEach(cell => {
+        cell.addEventListener("mouseover",()=>{
+            const colorChoice=Math.floor(Math.random()*rainbowColors.length);
+            cell.style.setProperty('background-color',rainbowColors[colorChoice])
+        })
+    })
+}
+
 sliderLabel.innerHTML=`Adjust grid size:<br>${sliderElement.value} X ${sliderElement.value}`;
 createGrid(sliderElement.value);
 
 sliderElement.addEventListener('input',() =>{
     gridContainer.innerHTML="";
-    let dimensions = sliderElement.value;
-    sliderLabel.innerHTML=`Adjust grid size:<br>${dimensions} X ${dimensions}`;
-    for (let i = 0; i < (dimensions*dimensions); i++) {
-        let cell = document.createElement('div');
-        cell.className='cells';
-        gridContainer.appendChild(cell);
-    }
-    gridContainer.style.setProperty(`grid-template-columns`,`repeat(${dimensions},1fr)`) ;
-    gridContainer.style.setProperty(`grid-template-rows`,`repeat(${dimensions},1fr)`) ;
-
-    const cellsInGrid = document.querySelectorAll(".cells");
-
-    cellsInGrid.forEach(cell =>{
-        cell.addEventListener("mouseover",()=>{
-            cell.classList.add("hover-cell");
-        })
-    })
-});
-
-clearBtn.addEventListener('click',() => {
-    gridContainer.innerHTML="";
+    sliderLabel.innerHTML=`Adjust grid size:<br>${sliderElement.value} X ${sliderElement.value}`;
     createGrid(sliderElement.value);
 });
 
-
-resetBtn.addEventListener('click',()=>{
-    gridContainer.innerHTML="";
-    sliderLabel.innerHTML=`Adjust grid size:<br>${sliderElement.defaultValue} X ${sliderElement.defaultValue}`;
-    sliderElement.value=sliderElement.defaultValue;
-    createGrid(sliderElement.defaultValue);
-})
+clearBtn.addEventListener('click',() => {clearGrid()});
+resetBtn.addEventListener('click',()=>{resetGrid()})
+rainbowBtn.addEventListener('click',()=>{rainbowModeOn()});
